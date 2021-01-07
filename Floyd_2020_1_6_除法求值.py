2103dbdcfc9944cfff9183b06bd581b67c6ccb86
FeiQing-Zhuang <724143471@qq.com>
2021-01-07 21:08:18 +0800

    1.7

---
+++ Floyd_2020_1_6_除法求值.py
@@ -0,0 +1,28 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        from collections import defaultdict
+        graph = defaultdict(int)
+        set1 = set()
+        for i in range(len(equations)):
+            a, b = equations[i]
+            graph[(a, b)] = values[i]
+            graph[(b, a)] = 1 / values[i]
+            set1.add(a)
+            set1.add(b)
+
+        # Floyd算法 求图中任意2点距离
+        arr = list(set1)
+        for k in arr:
+            for i in arr:
+                for j in arr:
+                    if graph[(i, k)] and graph[(k, j)]:
+                        graph[(i, j)] = graph[(i, k)] * graph[(k, j)]
+
+        res = []
+        for x, y in queries:
+            if graph[(x, y)]:
+                res.append(graph[(x, y)])
+            else:
+                res.append(-1)
+        return res
+
